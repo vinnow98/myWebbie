8ec9affeab3fb73ab7c73ca035fc232952c4bf29
Evin <evinwoon@gmail.com>
2023-11-07 11:22:57 +0100

    added resume

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let imageTracker = "i"
-function imageChange() {
+// let imageTracker = "i"
+// function imageChange() {
 
-    var image = document.getElementById("lightmode")
-    if (imageTracker == "i") {
-        image.src = "resources/lightOn.png"
-        imageTracker = "I"
-    } else {
-        image.src = "resources/lightOff.png"
-        imageTracker = "i"
-    }
-}
+//     var image = document.getElementById("lightmode")
+//     if (imageTracker == "i") {
+//         image.src = "resources/lightOn.png"
+//         imageTracker = "I"
+//     } else {
+//         image.src = "resources/lightOff.png"
+//         imageTracker = "i"
+//     }
+// }
